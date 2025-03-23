2105b22256d4a62663eafb0bdfc40ff7355fdc37
Nandinisemwal <nandini.semwal@edu.sait.ca>
2025-03-23 06:30:57 -0600

    updated the joke category

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ document.addEventListener("DOMContentLoaded", function () {
         titleElement.textContent = "JOKE OF THE DAY";
     })
 })
+//finished the title part//
+//start working on category part//
+function getJokeByCategory(category) {
+    fetch(`https://v2.jokeapi.dev/joke/${category}?safe-mode`)
+    .then(response => response.json())
+    .then(data => {
+        const jokeElement = document.getElementById('joke');
+        const titleElement = document.getElementById('title');
+
+        jokeElement.textContent = data.type === 'single' ? data.joke: `${data.setup} ... ${data.delivery}`;
+        //to display title as the joke category expect for pun//
+        if (data.category == `Pun`) {
+            titleElement.textContent = `A Random Pun`;
+            document.title = `A Random Pun`;
+        } else {
+            title.textContent = `A Random ${data.category} Joke`;
+            document.title = `A Random ${data.category} Joke`;
+        }
+    })
+}
