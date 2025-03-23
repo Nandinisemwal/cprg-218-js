7f4bffe8576968c791968adef84fe503d9ae97bd
Nandinisemwal <nandini.semwal@edu.sait.ca>
2025-03-23 05:37:06 -0600

    using getelement for jokes by category

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ fetch(`
 .then(data => {
     joke.textContent = data.joke || `${data.setup} ... ${data.delivery}`;
 })
+
+function getJokeByCategory(category) {
+    fetch(`https://v2.jokeapi.dev/joke/Category?safe-mode`) //to get jokes from a specific category//
+    .then(response => response.json())
+    .then(data => {
+        const jokeElement = document.getElementById('joke'); //using document.getElementById to get jokes by its ids//
+        const titleElement = document.getElementById('title'); 
+    })
+}
